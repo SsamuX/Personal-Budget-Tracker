8a21a76b53e6645ddf57cb4210d77497761bead9
SsamuX <sgachoki559@gmail.com>
2026-04-05 19:16:46 +0300

    Implement personal budget report functionality

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from input_handler import get_budget_data
+from calculator import calculate_total_expenses, calculate_balance
+from warnings import get_warning_message
 
+
+def main():
+    income, expenses = get_budget_data()
+
+    total_expenses = calculate_total_expenses(expenses)
+    balance = calculate_balance(income, total_expenses)
+    message = get_warning_message(balance)
+
+    print("\n----- PERSONAL BUDGET REPORT -----")
+    print("Income:", income)
+    print("Total Expenses:", total_expenses)
+    print("Balance:", balance)
+    print("Message:", message)
+
+
+main()
