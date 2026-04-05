c38d481cff2b63f0b76c669fa449696e6b6b875f
SsamuX <sgachoki559@gmail.com>
2026-04-05 19:15:23 +0300

    Update input_handler.py

--- input_handler.py
+++ input_handler.py
@@ -1,1 +1,9 @@
+def get_budget_data():
+    income = float(input("Enter total income: "))
+    food = float(input("Enter food expense: "))
+    transport = float(input("Enter transport expense: "))
+    rent = float(input("Enter rent expense: "))
+    entertainment = float(input("Enter entertainment expense: "))
 
+    expenses = [food, transport, rent, entertainment]
+    return income, expenses
